a57b28871009d2ed8a707df9c71813adf528ac00
kanchitbajaj8070 <31831699+kanchitbajaj8070@users.noreply.github.com>
2021-04-04 20:25:02 +0530

    i

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const app=express();
+const port=8000;
+
+app.listen(port,(error)=>{
+    if( error)
+    {
+        console.log(`Error in running the server:${error}`);
+        //new way of embedding strings in the string(interpolation of strings)
+        return;
+    }
+    console.log("Server is up and runnign succesfully")
+})
